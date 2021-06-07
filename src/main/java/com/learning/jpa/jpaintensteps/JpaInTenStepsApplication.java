3509ca3768790bc6812aa21eeb6d75fb061e180b
unknown <raghavprasad98@gmail.com>
2021-06-08 01:31:17 +0530

    first commit

---
+++ src/main/java/com/learning/jpa/jpaintensteps/JpaInTenStepsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.jpa.jpaintensteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaInTenStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaInTenStepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/jpa/jpaintensteps/JpaInTenStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.jpa.jpaintensteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaInTenStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
